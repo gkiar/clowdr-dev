73494594f0cb600605d484872a6b77fba2091c9b
Greg Kiar <gkiar07@gmail.com>
2017-12-06 10:25:25 -0500

    added basic metadata temp file

--- task/entrypoint.sh
+++ task/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Get task metadata from S3
-echo aws s3 cp --recursive "$1" /task/
 aws s3 cp --recursive "$1" /task/
 chmod +x -R /task/
 
 # Parse metadata
+cat /task/metadata.clwdr
 
 
 # Get input data, invocation, and descriptor from S3
+# aws s3 cp input data
 
 
 # Validate descriptor/invocation/input data combination
@@ -19,5 +20,5 @@ chmod +x -R /task/
 /task/execution.sh
 
 # Push output data and metadata back to S3
-
+# aws s3 cp outputs to input data loc
 
